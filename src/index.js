a364e2c2387300ff4cf3d02bf786e4cd7e64cbc0
Renato Bezerra <rbezerra@sealtelecom.com.br>
2020-04-15 01:55:11 -0300

    Fix default port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require('cors');
 const routes = require('../src/routes');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 
 app.disable('x-powered-by');
 
